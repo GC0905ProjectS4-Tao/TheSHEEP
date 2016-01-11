d0e6d3e328ca0c5e8a203afa8d1b9ef23f0e2398
dungthantai <dungthantai@gmail.com>
2016-01-11 17:23:36 +0700

    Session Bean

---
+++ src/java/Sess/SheepOrderFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sess;
+
+import Entity.SheepOrder;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author DTT
+ */
+@Stateless
+public class SheepOrderFacade extends AbstractFacade<SheepOrder> implements SheepOrderFacadeLocal {
+    @PersistenceContext(unitName = "TheSHEEPPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public SheepOrderFacade() {
+        super(SheepOrder.class);
+    }
+    
+}
